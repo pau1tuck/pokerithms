58291f039e52749583432a4e91917c1a149352cc
Paul Tuck <paul.tuck@oxon.org>
2023-07-25 13:42:26 +0700

    "2023-07-25_13:42+07"

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,17 +1,25 @@
 // import required libraries (fs)
 
-// Receive hands as inputs via process.stdin..., e.g. 4C 5S JH TS AD
-const hands = process.stdin.read().split(" ");
-
-// Split to array at spaces ['4C','5S','JH','TS','AD']
-
-// Empty array called emptyArray
+// Receive hands as inputs "4C 5S JH TS AD..."
+const _hands = process.stdin.read().split(" ");
+// ["4C", "5S", "JH", "TS", "AD"];
 
 // Slice to two variables in object {num:.., suit:..}
 
 // Process suit using switch..case..default
 
 // Push to a 2D array of hands of 5
+const _unused_variable = "shite";
 
 // hands.length()?
+console.log("apshiapple");
+const dog = [
+	{ num: ".", suit: "." },
+	{ num: ".", suit: "." },
+	{ num: ".", suit: "." },
+	{ num: ".", suit: "." },
+];
 
+// Rules
+// Royal Flush
+console.log("dog");
